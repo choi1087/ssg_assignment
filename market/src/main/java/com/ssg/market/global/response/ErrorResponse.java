f6ae1ffc1ea10eb9c3654d129ad5f2970b8174da
choi1087 <adcdm87@gmail.com>
2025-06-04 21:27:09 +0900

    add: 공통 response 클래스 생성 - 성공, 실패에 대한 응답

---
+++ market/src/main/java/com/ssg/market/global/response/ErrorResponse.java
@@ -0,0 +1,41 @@
+package com.ssg.market.global.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ssg.market.global.errorhandling.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
+
+@Getter
+@NoArgsConstructor
+public class ErrorResponse {
+    @Schema(defaultValue = "false", example = "false", description = "요청 성공 여부")
+    private boolean success = false;
+    @Schema(description = "실패 코드")
+    private String code;
+    @Schema(description = "실패 메시지")
+    private String message;
+    @Schema(description = "실패한 API 요청 URI")
+    private String path;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private ZonedDateTime timeStamp;
+
+    private ErrorResponse(String code, String message, String path) {
+        this.success = false;
+        this.code = code;
+        this.message = message;
+        this.path = path;
+        this.timeStamp = ZonedDateTime.now(TimeZone.getTimeZone("UTC").toZoneId());
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, String path) {
+        return new ErrorResponse(errorCode.getCode(), errorCode.getMessage(), path);
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, String message, String path) {
+        return new ErrorResponse(errorCode.getCode(), message, path);
+    }
+}
---
+++ market/src/main/java/com/ssg/market/global/response/SuccessResponse.java
@@ -0,0 +1,34 @@
+package com.ssg.market.global.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
+
+@Getter
+@Builder
+public class SuccessResponse<T> {
+    private boolean success;
+    private T data;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private ZonedDateTime timeStamp;
+
+    public static <T> SuccessResponse<T> response(boolean success, T data) {
+        return SuccessResponse.<T>builder()
+                .timeStamp(ZonedDateTime.now(TimeZone.getTimeZone("UTC").toZoneId()))
+                .success(success)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ResponseEntity<SuccessResponse<T>> success() {
+        return ResponseEntity.ok(response(true, null));
+    }
+
+    public static <T> ResponseEntity<SuccessResponse<T>> success(T data) {
+        return ResponseEntity.ok(response(true, data));
+    }
+}
