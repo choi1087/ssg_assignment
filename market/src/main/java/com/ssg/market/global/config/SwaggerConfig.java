6514674cc947d356298aefc0614521d54294d37b
choi1087 <adcdm87@gmail.com>
2025-06-08 21:01:20 +0900

    fix: swagger 설정 수정 - 로컬과 서버 환경에서의 설정 구분

--- market/src/main/java/com/ssg/market/global/config/SwaggerConfig.java
+++ market/src/main/java/com/ssg/market/global/config/SwaggerConfig.java
@@ -4,14 +4,23 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server-url}")
+    private String swaggerServerURL;
+
     @Bean
     public OpenAPI springOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url(swaggerServerURL)))
                 .info(new Info()
                         .title("SSG-ASSIGNMENT API")
                         .description("SSG 사전과제 API doc")
